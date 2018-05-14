fe108ae1efd2f05e2193aeaad1b5153f3343b4bd
yuckYew <yusukechayama@gmail.com>
2018-05-14 16:04:03 +0900

    End of Chapter5- User Logins

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,7 +1,10 @@
 from flask import render_template, flash, redirect, url_for, request
 from werkzeug.urls import url_parse
 from app import app
-from app.forms import LoginForm
+from app.forms import (
+    LoginForm,
+    RegistrationForm
+)
 from flask_login import (
     current_user, 
     login_user,
@@ -41,11 +44,24 @@ def login():
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('index')
-            return redirect(next_page)
-        return redirect(url_for('index'))
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
